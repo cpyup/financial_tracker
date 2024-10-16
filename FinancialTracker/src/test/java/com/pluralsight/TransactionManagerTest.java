e4ed3687facb94b5fcc8f8cd72ae4b9d7d543d6e
Cameron <cpolly@pgh.yearup.org>
2024-10-16 11:16:55 -0400

    Updated Test Cases

---
+++ FinancialTracker/src/test/java/com/pluralsight/TransactionManagerTest.java
@@ -0,0 +1,39 @@
+package com.pluralsight;
+
+import org.junit.jupiter.api.Test;
+
+
+import java.io.File;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionManagerTest {
+
+    @Test
+    void loadTransactions_properFile() {
+        String testFileName = "transactions.csv";
+        ArrayList<Transaction> testArray = new ArrayList<>();
+        TransactionManager.loadTransactions(testFileName,testArray);
+
+        assertFalse(testArray.isEmpty());
+    }
+
+    @Test
+    void loadTransactions_nonExistingFile() {
+        String testFileName = "non_existing_file.txt";
+        ArrayList<Transaction> testArray = new ArrayList<>();
+
+        TransactionManager.loadTransactions(testFileName,testArray);
+
+        // Check if the file has been created
+        File file = new File(testFileName);
+        assertTrue(file.exists());
+
+        // Clean up the test file
+        if(file.delete()){
+            System.out.println("Deleted Test File");
+        }
+    }
+}
