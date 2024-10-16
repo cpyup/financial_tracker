a0e54595baeec1001eae1987cd406f38e6d007b0
Cameron <cpolly@pgh.yearup.org>
2024-10-16 11:58:55 -0400

    Added Javadoc To Transaction Record

--- FinancialTracker/src/main/java/com/pluralsight/Transaction.java
+++ FinancialTracker/src/main/java/com/pluralsight/Transaction.java
@@ -3,6 +3,20 @@ package com.pluralsight;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+/**
+ * Represents a financial transaction with a date, time, description, vendor, and amount.
+ * <p>
+ * This record encapsulates all relevant information for a transaction and provides
+ * a custom string representation formatted for logging to data file.
+ * </p>
+ *
+ * @param date        the date of the transaction
+ * @param time        the time of the transaction
+ * @param description a brief description of the transaction
+ * @param vendor      the vendor associated with the transaction
+ * @param amount      the amount of the transaction, where a positive value indicates a deposit
+ *                    and a negative value indicates a payment
+ */
 public record Transaction(LocalDate date, LocalTime time, String description, String vendor, double amount) {
     @Override
     public String toString() {
